de7a302cb2552618593b4b6e35a17959ed1e1098
Mika Turunen <nalle@Mikas-iMac.Elisa>
2016-01-05 11:51:55 +0200

    Initial base commit - need to do a .gitignore run after this.

---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blogappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the blogappApp
+ */
+angular.module('blogappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
